5593338697658bf68641397ed8542547ff5aa5e9
Anurag JK <>
2021-07-04 14:33:35 +0530

    Added root required message

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 ARG=$1
+LOG="&> /dev/null"
 
-if [ ]; then
+if [ "$ARG" = "remove" ]; then
 	echo "Removing myAutoRotate..."
-	rm /usr/bin/myautorotate
+	rm /usr/bin/myautorotate 
 else
 	echo "Installing myAutoRotate..."
 	chmod +x myautorotate
 	cp myautorotate /usr/bin/
 fi
+
+if [ "$EUID" != 0 ]; then
+	echo "root permission required."
+	exit 0
+fi 
 
 echo "Done."
 
